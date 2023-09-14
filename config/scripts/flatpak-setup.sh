fc506fa1ca6c1771a27c8a3190b3720d8a3401c5
Patrick Csikos <pcsikos@zelikos.dev>
2023-09-14 03:34:36 -0400

    refactor!(flatpak): Migrate from yafti (#31)
    * feat!(deck): Disable yafti in main recipe
    
    * feat: Add flatpak remove list from bazzite
    
    Assuming they base it on what vanilla Silverblue ships with, which we'd
    want to remove too
    
    * feat(flatpak): Add core GNOME apps to install
    
    * feat(flatpak): Migrate Flatpak list from yafti
    
    * fix(flatpak): Remove comments from install list
    
    * feat(flatpak): Add Flatseal, PikaBackup
    
    * feat(flatpak): Import bazzite-flatpak-manager
    
    * feat(flatpak): bazzite -> zeliblue
    
    * fix(flatpak): Prefix flatpak-manager with zeliblue-
    
    * feat(flatpak): Adapt flatpak-manager.service from bazzite
    
    * feat(flatpak): Set up system-wide flathub
    
    * feat(flatpak): Add user-setup
    
    * feat(flatpak): Enable system and user flatpak services
    
    * fix: Try systemd global enable in Containerfile

---
+++ config/scripts/flatpak-setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+chmod +x /usr/bin/zeliblue-flatpak-manager
+chmod +x /usr/bin/zeliblue-user-setup
