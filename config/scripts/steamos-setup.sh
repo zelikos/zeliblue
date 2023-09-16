e5a447542a372ffc273252b0a62e5f52850768e7
zelikos <pcsikos@zelikos.dev>
2023-09-16 01:03:37 -0400

    feat(deck): Enable Steam autostart

--- config/scripts/steamos-setup.sh
+++ config/scripts/steamos-setup.sh
@@ -16,3 +16,12 @@ chmod +x /usr/bin/steamos-update
 chmod +x /usr/bin/zelideck-autologin
 chmod +x /usr/lib/os-session-select
 chmod +x /usr/share/gamescope-session/gamescope-session-script
+
+# Steam setup
+ln -s /usr/bin/steamos-logger /usr/bin/steamos-info
+ln -s /usr/bin/steamos-logger /usr/bin/steamos-notice
+ln -s /usr/bin/steamos-logger /usr/bin/steamos-warning
+sed -i 's@/usr/bin/steam@/usr/bin/zelideck-steam@g' /usr/share/applications/steam.desktop
+mkdir -p "/usr/etc/xdg/autostart"
+cp "/usr/share/applications/steam.desktop" "/usr/etc/xdg/autostart/steam.desktop"
+sed -i 's@/usr/bin/zelideck-steam %U@/usr/bin/zelideck-steam -silent %U@g' /usr/etc/xdg/autostart/steam.desktop
