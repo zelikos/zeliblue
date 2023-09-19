27ff03f83ec541f70a201eee14bdfef74c1fe53c
zelikos <pcsikos@zelikos.dev>
2023-09-19 14:45:05 -0400

    feat: Silverblue -> Zeliblue in os-release file

--- config/scripts/image-info.sh
+++ config/scripts/image-info.sh
@@ -9,3 +9,5 @@ set -oue pipefail
 IMAGE_VENDOR=zelikos
 mkdir -p /usr/etc/default
 echo -e "IMAGE_NAME=${IMAGE_NAME}\nIMAGE_VENDOR=${IMAGE_VENDOR}\nBASE_IMAGE_NAME=${BASE_IMAGE}\nFEDORA_MAJOR_VERSION=${OS_VERSION}" >> /usr/etc/default/zeliblue
+
+sed -i '/^PRETTY_NAME/s/Silverblue/Zeliblue/' /usr/lib/os-release
