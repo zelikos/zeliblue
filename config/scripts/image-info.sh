e575070970af0f189d07e055f613056404525453
zelikos <pcsikos@zelikos.dev>
2023-09-19 15:27:00 -0400

    fix(image-info): Check for base image for os-release

--- config/scripts/image-info.sh
+++ config/scripts/image-info.sh
@@ -10,4 +10,10 @@ IMAGE_VENDOR=zelikos
 mkdir -p /usr/etc/default
 echo -e "IMAGE_NAME=${IMAGE_NAME}\nIMAGE_VENDOR=${IMAGE_VENDOR}\nBASE_IMAGE_NAME=${BASE_IMAGE}\nFEDORA_MAJOR_VERSION=${OS_VERSION}" >> /usr/etc/default/zeliblue
 
-sed -i '/^PRETTY_NAME/s/Silverblue/Zeliblue/' /usr/lib/os-release
+if grep -q "silverblue" <<< "${BASE_IMAGE}"
+then
+  sed -i '/^PRETTY_NAME/s/Silverblue/Zeliblue/' /usr/lib/os-release
+elif grep -q "kinoite" <<< "${BASE_IMAGE}"
+then
+  sed -i '/^PRETTY_NAME/s/Kinoite/Zeliblue/' /usr/lib/os-release
+fi
