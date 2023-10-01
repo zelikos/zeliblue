b54fd991db4d5fff09470042469f66b0da5e4be3
zelikos <pcsikos@zelikos.dev>
2023-10-01 12:50:24 -0400

    fix: Point to correct directory

--- modules/flatpaks/flatpaks.sh
+++ modules/flatpaks/flatpaks.sh
@@ -4,10 +4,10 @@
 set -oue pipefail
 
 echo "Installing and enabling flatpaks module"
-cp -r "$CONFIG_DIRECTORY/modules/usr/bin/system-flatpak-setup" "/usr/bin/system-flatpak-setup"
-cp -r "$CONFIG_DIRECTORY/modules/usr/bin/user-flatpak-setup" "/usr/bin/user-flatpak-setup"
-cp -r "$CONFIG_DIRECTORY/modules/usr/lib/systemd/system/system-flatpak-setup.service" "/usr/lib/systemd/system/system-flatpak-setup.service"
-cp -r "$CONFIG_DIRECTORY/modules/usr/lib/systemd/user/user-flatpak-setup.service" "/usr/lib/systemd/user/user-flatpak-setup.service"
+cp -r "$CONFIG_DIRECTORY/files/modules/usr/bin/system-flatpak-setup" "/usr/bin/system-flatpak-setup"
+cp -r "$CONFIG_DIRECTORY/files/modules/usr/bin/user-flatpak-setup" "/usr/bin/user-flatpak-setup"
+cp -r "$CONFIG_DIRECTORY/files/modules/usr/lib/systemd/system/system-flatpak-setup.service" "/usr/lib/systemd/system/system-flatpak-setup.service"
+cp -r "$CONFIG_DIRECTORY/files/modules/usr/lib/systemd/user/user-flatpak-setup.service" "/usr/lib/systemd/user/user-flatpak-setup.service"
 
 systemctl enable system-flatpak-setup.service
 systemctl enable --global user-flatpak-setup.service
