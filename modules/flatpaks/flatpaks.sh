33e693e178510d1d218ebe8f0972627b2c4e5d2f
zelikos <pcsikos@zelikos.dev>
2023-10-01 12:58:19 -0400

    refactor: Move flatpak-setup files back into main usr dirs
    Unless this is made a bling module instead, this module is gonna have to
    work like yafti in that the necessary files need to be included in
    startingpoint directly.

--- modules/flatpaks/flatpaks.sh
+++ modules/flatpaks/flatpaks.sh
@@ -3,12 +3,6 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-echo "Installing and enabling flatpaks module"
-cp -r "$CONFIG_DIRECTORY/files/modules/usr/bin/system-flatpak-setup" "/usr/bin/system-flatpak-setup"
-cp -r "$CONFIG_DIRECTORY/files/modules/usr/bin/user-flatpak-setup" "/usr/bin/user-flatpak-setup"
-cp -r "$CONFIG_DIRECTORY/files/modules/usr/lib/systemd/system/system-flatpak-setup.service" "/usr/lib/systemd/system/system-flatpak-setup.service"
-cp -r "$CONFIG_DIRECTORY/files/modules/usr/lib/systemd/user/user-flatpak-setup.service" "/usr/lib/systemd/user/user-flatpak-setup.service"
-
+echo "Enabling flatpaks module"
 systemctl enable system-flatpak-setup.service
 systemctl enable --global user-flatpak-setup.service
-
