b688cb95406711164ea9c396ba032244cb30a07b
zelikos <pcsikos@zelikos.dev>
2023-10-01 12:46:50 -0400

    fix: Copy system and user scripts separately

--- modules/flatpaks/flatpaks.sh
+++ modules/flatpaks/flatpaks.sh
@@ -4,7 +4,8 @@
 set -oue pipefail
 
 echo "Installing and enabling flatpaks module"
-cp -r "$CONFIG_DIRECTORY/modules/usr/bin/{system,user}-flatpak-setup" "/usr/bin/{system,user}-flatpak-setup"
+cp -r "$CONFIG_DIRECTORY/modules/usr/bin/system-flatpak-setup" "/usr/bin/system-flatpak-setup"
+cp -r "$CONFIG_DIRECTORY/modules/usr/bin/user-flatpak-setup" "/usr/bin/user-flatpak-setup"
 cp -r "$CONFIG_DIRECTORY/modules/usr/lib/systemd/system/system-flatpak-setup.service" "/usr/lib/systemd/system/system-flatpak-setup.service"
 cp -r "$CONFIG_DIRECTORY/modules/usr/lib/systemd/user/user-flatpak-setup.service" "/usr/lib/systemd/user/user-flatpak-setup.service"
 
