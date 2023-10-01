b8766e4d101f02e7455c6762022290c2201192e5
zelikos <pcsikos@zelikos.dev>
2023-10-01 12:43:23 -0400

    feat: Enable flatpaks module

--- modules/flatpaks/flatpaks.sh
+++ modules/flatpaks/flatpaks.sh
@@ -3,7 +3,11 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
+echo "Installing and enabling flatpaks module"
 cp -r "$CONFIG_DIRECTORY/modules/usr/bin/{system,user}-flatpak-setup" "/usr/bin/{system,user}-flatpak-setup"
 cp -r "$CONFIG_DIRECTORY/modules/usr/lib/systemd/system/system-flatpak-setup.service" "/usr/lib/systemd/system/system-flatpak-setup.service"
 cp -r "$CONFIG_DIRECTORY/modules/usr/lib/systemd/user/user-flatpak-setup.service" "/usr/lib/systemd/user/user-flatpak-setup.service"
+
+systemctl enable system-flatpak-setup.service
+systemctl enable --global user-flatpak-setup.service
 
