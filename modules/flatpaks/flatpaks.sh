96574c99b42e19fd10b4d6695248ed60426e6a94
zelikos <pcsikos@zelikos.dev>
2023-10-01 15:06:37 -0400

    fix: Build install and removal lists correctly

--- modules/flatpaks/flatpaks.sh
+++ modules/flatpaks/flatpaks.sh
@@ -18,30 +18,38 @@ get_yaml_array SYSTEM_REMOVE '.system.remove[]' "$1"
 get_yaml_array USER_INSTALL '.user.install[]' "$1"
 get_yaml_array USER_REMOVE '.user.remove[]' "$1"
 
-if [[ ${#SYSTEM_INSTALL[0]} -gt 0 ]]; then
+echo "Creating system Flatpak install list"
+if [[ ${#SYSTEM_INSTALL[@]} -gt 0 ]]; then
     rm -f $SYS_INSTALL_LIST && touch $SYS_INSTALL_LIST
-    for flatpak in "${#SYSTEM_INSTALL[0]}"; do
+    for flatpak in "${SYSTEM_INSTALL[@]}"; do
+        echo $flatpak
         echo $flatpak >> $SYS_INSTALL_LIST
     done
 fi
 
-if [[ ${#SYSTEM_REMOVE[0]} -gt 0 ]]; then
+echo "Creating system Flatpak removals list"
+if [[ ${#SYSTEM_REMOVE[@]} -gt 0 ]]; then
     rm -f $SYS_REMOVE_LIST && touch $SYS_REMOVE_LIST
-    for flatpak in "${#SYSTEM_REMOVE[0]}"; do
+    for flatpak in "${SYSTEM_REMOVE[@]}"; do
+        echo $flatpak
         echo $flatpak >> $SYS_REMOVE_LIST
     done
 fi
 
-if [[ ${#USER_INSTALL[0]} -gt 0 ]]; then
+echo "Creating user Flatpak install list"
+if [[ ${#USER_INSTALL[@]} -gt 0 ]]; then
     rm -f $USER_INSTALL_LIST && touch $USER_INSTALL_LIST
-    for flatpak in "${#USER_INSTALL[0]}"; do
+    for flatpak in "${USER_INSTALL[@]}"; do
+        echo $flatpak
         echo $flatpak >> $USER_INSTALL_LIST
     done
 fi
 
-if [[ ${#USER_REMOVE[0]} -gt 0 ]]; then
+echo "Creating user Flatpak removals list"
+if [[ ${#USER_REMOVE[@]} -gt 0 ]]; then
     rm -f $USER_REMOVE_LIST && touch $USER_REMOVE_LIST
-    for flatpak in "${#USER_REMOVE[0]}"; do
+    for flatpak in "${USER_REMOVE[@]}"; do
+        echo $flatpak
         echo $flatpak >> $USER_REMOVE_LIST
     done
 fi
