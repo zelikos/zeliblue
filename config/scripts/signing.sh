66ba60c198036980401b41cf3097d21fea4bdfda
Patrick Csikos <pcsikos@zelikos.dev>
2023-10-02 18:30:45 -0400

    refactor: Consolidate image-info.sh into signing.sh (#53)
    Some image info is set up in signing.sh now, so consolidating what I had
    in image-info into there makes sense.

--- config/scripts/signing.sh
+++ config/scripts/signing.sh
@@ -3,7 +3,9 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-source /usr/etc/default/zeliblue
+IMAGE_VENDOR=zelikos
+IMAGE_TAG=latest # Leaving as a variable in case of future changes
+IMAGE_INFO=/usr/share/ublue-os/image-info.json
 
 echo "Setting up container signing in policy.json and cosign.yaml for $IMAGE_NAME"
 echo "Registry to write: $IMAGE_REGISTRY"
@@ -26,7 +28,27 @@ yq -i -o=j '.transports.docker |=
 + .' "$FILE"
 
 IMAGE_REF="ostree-image-signed:docker://$IMAGE_REGISTRY/$IMAGE_NAME"
-printf '{\n"image-ref": "'"$IMAGE_REF"'",\n"image-tag": "latest"\n}' > /usr/share/ublue-os/image-info.json
+# printf '{\n"image-ref": "'"$IMAGE_REF"'",\n"image-tag": "latest"\n}' > /usr/share/ublue-os/image-info.json
+
+touch $IMAGE_INFO
+cat > $IMAGE_INFO <<EOF
+{
+    "image-name": "$IMAGE_NAME",
+    "image-flavor": "$BASE_IMAGE",
+    "image-vendor": "$IMAGE_VENDOR",
+    "image-ref": "$IMAGE_REF",
+    "image-tag": "$IMAGE_TAG",
+    "fedora-version": "$OS_VERSION"
+}
+EOF
+
+if grep -q "silverblue" <<< "${BASE_IMAGE}"
+then
+  sed -i '/^PRETTY_NAME/s/Silverblue/Zeliblue/' /usr/lib/os-release
+elif grep -q "kinoite" <<< "${BASE_IMAGE}"
+then
+  sed -i '/^PRETTY_NAME/s/Kinoite/Zeliblue Plasma/' /usr/lib/os-release
+fi
 
 cp /usr/etc/containers/registries.d/ublue-os.yaml /usr/etc/containers/registries.d/"$IMAGE_VENDOR".yaml
 sed -i "s ghcr.io/ublue-os $IMAGE_REGISTRY g" /usr/etc/containers/registries.d/"$IMAGE_VENDOR".yaml
