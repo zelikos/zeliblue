55299b14012999d261bdc3c6cae2449fa65f97b3
zelikos <pcsikos@zelikos.dev>
2024-03-22 23:22:01 -0400

    feat: Update image-info.sh to generate image-info.json
    Needed since the blue-build signing module no longer does this

--- config/scripts/image-info.sh
+++ config/scripts/image-info.sh
@@ -5,6 +5,10 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
+IMAGE_INFO="/usr/share/zeliblue/image-info.json"
+IMAGE_VENDOR="zelikos"
+IMAGE_REF="ostree-image-signed:docker://$IMAGE_VENDOR/$IMAGE_NAME"
+
 if grep -q "silverblue" <<< "${BASE_IMAGE}"
 then
   sed -i '/^PRETTY_NAME/s/Silverblue/Zeliblue/' /usr/lib/os-release
@@ -12,3 +16,15 @@ elif grep -q "kinoite" <<< "${BASE_IMAGE}"
 then
   sed -i '/^PRETTY_NAME/s/Kinoite/Zeliblue Plasma/' /usr/lib/os-release
 fi
+
+cat > $IMAGE_INFO <<EOF
+{
+  "image-name": "$IMAGE_NAME",
+  "image-flavor": "$IMAGE_FLAVOR",
+  "image-vendor": "$IMAGE_VENDOR",
+  "image-ref": "$IMAGE_REF",
+  "image-tag":"$IMAGE_TAG",
+  "base-image-name": "$BASE_IMAGE",
+  "fedora-version": "$FEDORA_MAJOR_VERSION"
+}
+EOF
