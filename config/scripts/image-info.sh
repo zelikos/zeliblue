4b7a540c9791061652f7e1ba6db71d699a4210bc
Patrick Csikos <pcsikos@zelikos.dev>
2024-03-23 04:56:28 -0400

    feat: Update image-info.sh for more complete image info (#212)
    Fixes #211

--- config/scripts/image-info.sh
+++ config/scripts/image-info.sh
@@ -5,10 +5,30 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-if grep -q "silverblue" <<< "${BASE_IMAGE}"
-then
-  sed -i '/^PRETTY_NAME/s/Silverblue/Zeliblue/' /usr/lib/os-release
-elif grep -q "kinoite" <<< "${BASE_IMAGE}"
-then
+IMAGE_INFO="/usr/share/zeliblue/image-info.json"
+IMAGE_VENDOR="zelikos"
+IMAGE_REF="ostree-image-signed:docker://ghcr.io/$IMAGE_VENDOR/$IMAGE_NAME"
+IMAGE_FLAVOR=""
+IMAGE_TAG="latest"
+
+if grep -q "kinoite" <<< "${BASE_IMAGE}"; then
   sed -i '/^PRETTY_NAME/s/Kinoite/Zeliblue Plasma/' /usr/lib/os-release
+  sed -i '/^VERSION/s/Kinoite/Zeliblue Plasma/' /usr/lib/os-release
+  IMAGE_FLAVOR="kinoite"
+else
+  sed -i '/^PRETTY_NAME/s/Silverblue/Zeliblue/' /usr/lib/os-release
+  sed -i '/^VERSION/s/Silverblue/Zeliblue/' /usr/lib/os-release
+  IMAGE_FLAVOR="silverblue"
 fi
+
+cat > $IMAGE_INFO <<EOF
+{
+  "image-name": "$IMAGE_NAME",
+  "image-flavor": "$IMAGE_FLAVOR",
+  "image-vendor": "$IMAGE_VENDOR",
+  "image-ref": "$IMAGE_REF",
+  "image-tag":"$IMAGE_TAG",
+  "base-image-name": "$BASE_IMAGE",
+  "fedora-version": "$OS_VERSION"
+}
+EOF
