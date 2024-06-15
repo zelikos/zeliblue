a4199f8ac3139ac17f68ce1388a3f8c6bc1eea2b
zelikos <pcsikos@zelikos.dev>
2024-06-15 16:22:41 -0400

    build: Add build.yml and build.sh from ublue image template

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+RELEASE="$(rpm -E %fedora)"
+
+
+### Install packages
+
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+
+# this installs a package from fedora repos
+# rpm-ostree install screen
+
+#### Example for enabling a System Unit File
