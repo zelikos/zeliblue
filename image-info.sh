32697764caf6c5fc51f8be1555649bb9138e6150
zelikos <pcsikos@zelikos.dev>
2024-06-15 17:54:05 -0400

    fix(image-info): Set IMAGE_NAME

--- image-info.sh
+++ image-info.sh
@@ -5,21 +5,22 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-IMAGE_INFO="/usr/share/zeliblue/image-info.json"
-IMAGE_VENDOR="zelikos"
-IMAGE_REF="ostree-image-signed:docker://ghcr.io/$IMAGE_VENDOR/$IMAGE_NAME"
-IMAGE_FLAVOR=""
-IMAGE_TAG="latest"
-
 if grep -q "kinoite" <<< "${SOURCE_IMAGE}"; then
   sed -i '/^PRETTY_NAME/s/Kinoite/Zeliblue Plasma/' /usr/lib/os-release
   sed -i '/^VERSION/s/Kinoite/Zeliblue Plasma/' /usr/lib/os-release
   IMAGE_FLAVOR="kinoite"
+  IMAGE_NAME="zeliblue-plasma"
 else
   sed -i '/^PRETTY_NAME/s/Silverblue/Zeliblue GNOME/' /usr/lib/os-release
   sed -i '/^VERSION/s/Silverblue/Zeliblue GNOME/' /usr/lib/os-release
   IMAGE_FLAVOR="silverblue"
+  IMAGE_NAME="zeliblue-gnome"
 fi
+
+IMAGE_INFO="/usr/share/zeliblue/image-info.json"
+IMAGE_VENDOR="zelikos"
+IMAGE_REF="ostree-image-signed:docker://ghcr.io/$IMAGE_VENDOR/$IMAGE_NAME"
+IMAGE_TAG="latest"
 
 cat > $IMAGE_INFO <<EOF
 {
