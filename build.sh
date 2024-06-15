8e5cc55b1251139bd8318b01fbdc666ddd222f73
zelikos <pcsikos@zelikos.dev>
2024-06-15 16:31:52 -0400

    feat: Add base packages

--- build.sh
+++ build.sh
@@ -13,6 +13,4 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-# rpm-ostree install screen
-
-#### Example for enabling a System Unit File
+rpm-ostree install fish gcc
