d024070815f57f92d1f0f95f3ab860ae946630df
zelikos <pcsikos@zelikos.dev>
2024-07-29 23:08:26 -0400

    feat: Adapt image-info os-release stuff from Bazzite

--- files/scripts/image-info.sh
+++ files/scripts/image-info.sh
@@ -5,6 +5,12 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
+IMAGE_PRETTY_NAME="Zeliblue"
+HOME_URL="https://github.com/zelikos/zeliblue"
+DOCUMENTATION_URL=$HOME_URL
+SUPPORT_URL="https://github.com/zelikos/zeliblue/issues"
+BUG_REPORT_URL=$SUPPORT_URL
+
 IMAGE_INFO="/usr/share/zeliblue/image-info.json"
 IMAGE_VENDOR="zelikos"
 IMAGE_REF="ostree-image-signed:docker://ghcr.io/$IMAGE_VENDOR/$IMAGE_NAME"
@@ -12,12 +18,8 @@ IMAGE_FLAVOR=""
 IMAGE_TAG="latest"
 
 if grep -q "kinoite" <<< "${BASE_IMAGE}"; then
-  sed -i '/^PRETTY_NAME/s/Kinoite/Zeliblue Plasma/' /usr/lib/os-release
-  sed -i '/^VERSION/s/Kinoite/Zeliblue Plasma/' /usr/lib/os-release
   IMAGE_FLAVOR="kinoite"
 else
-  sed -i '/^PRETTY_NAME/s/Silverblue/Zeliblue/' /usr/lib/os-release
-  sed -i '/^VERSION/s/Silverblue/Zeliblue/' /usr/lib/os-release
   IMAGE_FLAVOR="silverblue"
 fi
 
@@ -32,3 +34,18 @@ cat > $IMAGE_INFO <<EOF
   "fedora-version": "$OS_VERSION"
 }
 EOF
+
+# OS Release File
+sed -i "s/^VARIANT_ID=.*/VARIANT_ID=$IMAGE_NAME/" /usr/lib/os-release
+sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME=\"Zeliblue (FROM Fedora ${BASE_IMAGE^} $FEDORA_MAJOR_VERSION)\"/" /usr/lib/os-release
+sed -i "s/^NAME=.*/NAME=\"$IMAGE_PRETTY_NAME\"/" /usr/lib/os-release
+sed -i "s|^HOME_URL=.*|HOME_URL=\"$HOME_URL\"|" /usr/lib/os-release
+sed -i "s|^DOCUMENTATION_URL=.*|DOCUMENTATION_URL=\"$DOCUMENTATION_URL\"|" /usr/lib/os-release
+sed -i "s|^SUPPORT_URL=.*|SUPPORT_URL=\"$SUPPORT_URL\"|" /usr/lib/os-release
+sed -i "s|^BUG_REPORT_URL=.*|BUG_REPORT_URL=\"$BUG_REPORT_URL\"|" /usr/lib/os-release
+# sed -i "s|^CPE_NAME=\"cpe:/o:fedoraproject:fedora|CPE_NAME=\"cpe:/o:universal-blue:${IMAGE_PRETTY_NAME,}|" /usr/lib/os-release
+sed -i "s/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME=\"${IMAGE_PRETTY_NAME,}\"/" /usr/lib/os-release
+sed -i "s/^ID=fedora/ID=${IMAGE_PRETTY_NAME,}\nID_LIKE=\"rhel centos fedora\"/" /usr/lib/os-release
+# sed -i "s/^LOGO=.*/LOGO=$LOGO_ICON/" /usr/lib/os-release
+# sed -i "s/^ANSI_COLOR=.*/ANSI_COLOR=\"$LOGO_COLOR\"/" /usr/lib/os-release
+sed -i "/^REDHAT_BUGZILLA_PRODUCT=/d; /^REDHAT_BUGZILLA_PRODUCT_VERSION=/d; /^REDHAT_SUPPORT_PRODUCT=/d; /^REDHAT_SUPPORT_PRODUCT_VERSION=/d" /usr/lib/os-release
