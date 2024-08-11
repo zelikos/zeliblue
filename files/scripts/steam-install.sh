18eef759d8021553f0d1da8d2a1542d703e1926c
zelikos <pcsikos@zelikos.dev>
2024-08-11 18:26:40 -0400

    chore: Add rpm-ostree overrides from Bazzite

--- files/scripts/steam-install.sh
+++ files/scripts/steam-install.sh
@@ -4,6 +4,136 @@ set -oue pipefail
 
 # Based on Bazzite's Containerfile: https://github.com/ublue-os/bazzite/blob/main/Containerfile
 
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    vulkan-loader \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    alsa-lib \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    gnutls \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    glib2 \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    nspr \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    nss-softokn \
+    nss-softokn-freebl \
+    nss-util \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    atk \
+    at-spi2-atk \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    libaom \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    gstreamer1 \
+    gstreamer1-plugins-base \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    libdecor \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    libtirpc \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    libuuid \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    libblkid \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    libmount \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    cups-libs \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    libinput \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    libopenmpt \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    llvm-libs \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    zlib-ng-compat \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    fontconfig \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    pciutils-libs \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    libdrm \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    libX11 \
+    libX11-common \
+    libX11-xcb \
+    || true && \
+rpm-ostree override replace \
+--experimental \
+--from repo=updates \
+    libv4l \
+    || true && \
+rpm-ostree override remove \
+    glibc32 \
+    || true
+
 # Enable bazzite-multilib for their build of Mesa, and for extest later.
 echo "Adding bazzite-multilib repository"
 
