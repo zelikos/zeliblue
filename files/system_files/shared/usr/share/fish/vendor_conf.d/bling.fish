026c7e557f2c1e22f6a15c31de0d36afcca6b209
zelikos <pcsikos@zelikos.dev>
2024-08-12 19:36:59 -0400

    fix(zeliblue-cli): Ensure Homebrew is loaded correctly for fish

--- files/system_files/shared/usr/share/fish/vendor_conf.d/bling.fish
+++ files/system_files/shared/usr/share/fish/vendor_conf.d/bling.fish
@@ -1,3 +1,14 @@
+# Ensure Homebrew is loaded properly (See: https://github.com/orgs/Homebrew/discussions/4412#discussioncomment-8651316)
+if test -d /var/home/linuxbrew/.linuxbrew
+    set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
+	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
+	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/Homebrew"
+
+	fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
+    ! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
+    ! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH;
+end
+
 if status is-interactive
   if command -q atuin
     atuin init fish | source
