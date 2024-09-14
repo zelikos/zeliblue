a3e1869d0dcf46bb1aa04355766e41c3d304c4de
zelikos <pcsikos@zelikos.dev>
2024-09-13 21:09:45 -0400

    feat: Integrate fastfetch config from Bluefin

--- files/system_files/shared/usr/share/fish/vendor_conf.d/zeliblue-fastfetch.fish
+++ files/system_files/shared/usr/share/fish/vendor_conf.d/zeliblue-fastfetch.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/fish
 #shellcheck disable=all
-alias fastfetch="fastfetch --logo fedora-silverblue"
+alias fastfetch="/usr/bin/fastfetch --logo fedora-silverblue -c /usr/share/zeliblue/fastfetch/zeliblue.jsonc"
