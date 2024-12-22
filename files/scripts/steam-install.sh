d80f33ee1188c7a2579285efcfcf3439291038ce
zelikos <pcsikos@zelikos.dev>
2024-12-22 13:43:01 -0500

    refactor(deck): Install Steam & dependencies with rpm-ostree module

--- files/scripts/steam-install.sh
+++ files/scripts/steam-install.sh
@@ -39,41 +39,41 @@ curl -Lo /etc/yum.repos.d/_copr_kylegospo-bazzite-multilib.repo https://copr.fed
 #       bluez-libs \
 #       xorg-x11-server-Xwayland
 
-echo "Installing other Steam dependencies"
+# echo "Installing other Steam dependencies"
 
-rpm-ostree install \
-  jupiter-sd-mounting-btrfs \
-  at-spi2-core.i686 \
-  atk.i686 \
-  vulkan-loader.i686 \
-  alsa-lib.i686 \
-  fontconfig.i686 \
-  gtk2.i686 \
-  libICE.i686 \
-  libnsl.i686 \
-  libxcrypt-compat.i686 \
-  libpng12.i686 \
-  libXext.i686 \
-  libXinerama.i686 \
-  libXtst.i686 \
-  libXScrnSaver.i686 \
-  NetworkManager-libnm.i686 \
-  nss.i686 \
-  pulseaudio-libs.i686 \
-  libcurl.i686 \
-  systemd-libs.i686 \
-  libva.i686 \
-  libvdpau.i686 \
-  libdbusmenu-gtk3.i686 \
-  libatomic.i686 \
-  pipewire-alsa.i686 \
-  gobject-introspection \
-  gamescope \
-  gamescope-shaders \
-  gamescope-session-plus \
-  gamescope-session-steam \
-  gamescope-libs.i686 \
-  steam
+# rpm-ostree install \
+#   jupiter-sd-mounting-btrfs \
+#   at-spi2-core.i686 \
+#   atk.i686 \
+#   vulkan-loader.i686 \
+#   alsa-lib.i686 \
+#   fontconfig.i686 \
+#   gtk2.i686 \
+#   libICE.i686 \
+#   libnsl.i686 \
+#   libxcrypt-compat.i686 \
+#   libpng12.i686 \
+#   libXext.i686 \
+#   libXinerama.i686 \
+#   libXtst.i686 \
+#   libXScrnSaver.i686 \
+#   NetworkManager-libnm.i686 \
+#   nss.i686 \
+#   pulseaudio-libs.i686 \
+#   libcurl.i686 \
+#   systemd-libs.i686 \
+#   libva.i686 \
+#   libvdpau.i686 \
+#   libdbusmenu-gtk3.i686 \
+#   libatomic.i686 \
+#   pipewire-alsa.i686 \
+#   gobject-introspection \
+#   gamescope \
+#   gamescope-shaders \
+#   gamescope-session-plus \
+#   gamescope-session-steam \
+#   gamescope-libs.i686 \
+#   steam
 
 # Add bootstrap_steam.tar.gz used by gamescope-session (Thanks GE & Nobara Project!)
 mkdir -p /usr/share/gamescope-session-plus/
