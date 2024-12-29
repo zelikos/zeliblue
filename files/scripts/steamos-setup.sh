6e28b7568ef935b419fae69b85a325fdd3dd8303
zelikos <pcsikos@zelikos.dev>
2024-12-29 13:02:24 -0500

    feat(deck): Add Steam Deck firmware files

--- files/scripts/steamos-setup.sh
+++ files/scripts/steamos-setup.sh
@@ -5,12 +5,45 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-# Steam setup
+# Deck firmware
+
+JUPITER_FIRMWARE_VERSION="jupiter-20240917.1"
+
+mkdir -p /tmp/linux-firmware-neptune
+curl -Lo /tmp/linux-firmware-neptune/cs35l41-dsp1-spk-cali.bin https://gitlab.com/evlaV/linux-firmware-neptune/-/raw/"${JUPITER_FIRMWARE_VERSION}"/cs35l41-dsp1-spk-cali.bin
+curl -Lo /tmp/linux-firmware-neptune/cs35l41-dsp1-spk-cali.wmfw https://gitlab.com/evlaV/linux-firmware-neptune/-/raw/"${JUPITER_FIRMWARE_VERSION}"/cs35l41-dsp1-spk-cali.wmfw
+curl -Lo /tmp/linux-firmware-neptune/cs35l41-dsp1-spk-prot.bin https://gitlab.com/evlaV/linux-firmware-neptune/-/raw/"${JUPITER_FIRMWARE_VERSION}"/cs35l41-dsp1-spk-prot.bin
+curl -Lo /tmp/linux-firmware-neptune/cs35l41-dsp1-spk-prot.wmfw https://gitlab.com/evlaV/linux-firmware-neptune/-/raw/"${JUPITER_FIRMWARE_VERSION}"/cs35l41-dsp1-spk-prot.wmfw
+xz --check=crc32 /tmp/linux-firmware-neptune/cs35l41-dsp1-spk-{cali.bin,cali.wmfw,prot.bin,prot.wmfw}
+mv -vf /tmp/linux-firmware-neptune/* /usr/lib/firmware/cirrus/
+rm -rf /tmp/linux-firmware-neptune
+mkdir -p /tmp/linux-firmware-galileo
+curl https://gitlab.com/evlaV/linux-firmware-neptune/-/archive/"${JUPITER_FIRMWARE_VERSION}"/linux-firmware-neptune-"${JUPITER_FIRMWARE_VERSION}".tar.gz?path=ath11k/QCA206X -o /tmp/linux-firmware-galileo/ath11k.tar.gz
+tar --strip-components 1 --no-same-owner --no-same-permissions --no-overwrite-dir -xvf /tmp/linux-firmware-galileo/ath11k.tar.gz -C /tmp/linux-firmware-galileo
+xz --check=crc32 /tmp/linux-firmware-galileo/ath11k/QCA206X/hw2.1/*
+rm -f /usr/lib/firmware/ath11k/QCA206X/*
+rm -rf /usr/lib/firmware/ath11k/QCA2066
+mv -vf /tmp/linux-firmware-galileo/ath11k/QCA206X /usr/lib/firmware/ath11k/QCA206X
+rm -rf /tmp/linux-firmware-galileo/ath11k
+rm -rf /tmp/linux-firmware-galileo/ath11k.tar.gz
+ln -s QCA206X /usr/lib/firmware/ath11k/QCA2066
+curl -Lo /tmp/linux-firmware-galileo/hpbtfw21.tlv https://gitlab.com/evlaV/linux-firmware-neptune/-/raw/"${JUPITER_FIRMWARE_VERSION}"/qca/hpbtfw21.tlv
+curl -Lo /tmp/linux-firmware-galileo/hpnv21.309 https://gitlab.com/evlaV/linux-firmware-neptune/-/raw/"${JUPITER_FIRMWARE_VERSION}"/qca/hpnv21.309
+curl -Lo /tmp/linux-firmware-galileo/hpnv21.bin https://gitlab.com/evlaV/linux-firmware-neptune/-/raw/"${JUPITER_FIRMWARE_VERSION}"/qca/hpnv21.bin
+curl -Lo /tmp/linux-firmware-galileo/hpnv21g.309 https://gitlab.com/evlaV/linux-firmware-neptune/-/raw/"${JUPITER_FIRMWARE_VERSION}"/qca/hpnv21g.309
+curl -Lo /tmp/linux-firmware-galileo/hpnv21g.bin https://gitlab.com/evlaV/linux-firmware-neptune/-/raw/"${JUPITER_FIRMWARE_VERSION}"/qca/hpnv21g.bin
+xz --check=crc32 /tmp/linux-firmware-galileo/*
+mv -vf /tmp/linux-firmware-galileo/* /usr/lib/firmware/qca/
+rm -rf /tmp/linux-firmware-galileo
+
+# Dock updater
 git clone https://gitlab.com/evlaV/jupiter-dock-updater-bin.git \
         --depth 1 \
         /tmp/jupiter-dock-updater-bin
 mv -v /tmp/jupiter-dock-updater-bin/packaged/usr/lib/jupiter-dock-updater /usr/libexec/jupiter-dock-updater
 rm -rf /tmp/jupiter-dock-updater-bin
+
+# Steam setup
 ln -s /usr/bin/steamos-logger /usr/bin/steamos-info
 ln -s /usr/bin/steamos-logger /usr/bin/steamos-notice
 ln -s /usr/bin/steamos-logger /usr/bin/steamos-warning
