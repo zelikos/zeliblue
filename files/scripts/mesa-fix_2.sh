38fcabf87815e3e17750698873d2c4d940acffc9
zelikos <pcsikos@zelikos.dev>
2024-12-30 15:00:21 -0500

    fix(deck): Add standard script header to mesa-fix_2.sh

--- files/scripts/mesa-fix_2.sh
+++ files/scripts/mesa-fix_2.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
 rsync -a /tmp/mesa-fix64/ /usr/lib64/
 rsync -a /tmp/mesa-fix32/ /usr/lib/
 rm -rf /tmp/mesa-fix64
