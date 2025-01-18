a722749cf7d3e39e323d13b192606107abcec7e2
zelikos <pcsikos@zelikos.dev>
2025-01-18 12:40:40 -0500

    fix(deck,initramfs): Remove KERNEL_FLAVOR check

--- modules/initramfs/initramfs.sh
+++ modules/initramfs/initramfs.sh
@@ -2,11 +2,7 @@
 
 set -eoux pipefail
 
-if [[ "${KERNEL_FLAVOR}" == "surface" ]]; then
-    KERNEL_SUFFIX="surface"
-else
-    KERNEL_SUFFIX=""
-fi
+KERNEL_SUFFIX=""
 
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
 /usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
