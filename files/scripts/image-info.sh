a0d67212fd43297e3aaeb42d3a9959bfd0c67910
zelikos <pcsikos@zelikos.dev>
2025-02-01 09:54:44 -0500

    fix(image-info): Don't change ID from fedora for now
    Changing the ID causes issues with bootc-image-builder, so for now we'll
    keep the ID as fedora

--- files/scripts/image-info.sh
+++ files/scripts/image-info.sh
@@ -50,10 +50,10 @@ sed -i "s|^SUPPORT_URL=.*|SUPPORT_URL=\"$SUPPORT_URL\"|" /usr/lib/os-release
 sed -i "s|^BUG_REPORT_URL=.*|BUG_REPORT_URL=\"$BUG_REPORT_URL\"|" /usr/lib/os-release
 sed -i "s|^CPE_NAME=\"cpe:/o:fedoraproject:fedora|CPE_NAME=\"cpe:/o:zelikos:${BASIC_NAME,}|" /usr/lib/os-release
 sed -i "s/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME=\"${BASIC_NAME,}\"/" /usr/lib/os-release
-sed -i "s/^ID=fedora/ID=zeliblue\nID_LIKE=\"fedora\"/" /usr/lib/os-release
+# sed -i "s/^ID=fedora/ID=zeliblue\nID_LIKE=\"fedora\"/" /usr/lib/os-release
 # sed -i "s/^LOGO=.*/LOGO=$LOGO_ICON/" /usr/lib/os-release
 # sed -i "s/^ANSI_COLOR=.*/ANSI_COLOR=\"$LOGO_COLOR\"/" /usr/lib/os-release
 sed -i "/^REDHAT_BUGZILLA_PRODUCT=/d; /^REDHAT_BUGZILLA_PRODUCT_VERSION=/d; /^REDHAT_SUPPORT_PRODUCT=/d; /^REDHAT_SUPPORT_PRODUCT_VERSION=/d" /usr/lib/os-release
 
 # Fix issues caused by ID no longer being fedora
-sed -i "s/^EFIDIR=.*/EFIDIR=\"fedora\"/" /usr/sbin/grub2-switch-to-blscfg
+# sed -i "s/^EFIDIR=.*/EFIDIR=\"fedora\"/" /usr/sbin/grub2-switch-to-blscfg
