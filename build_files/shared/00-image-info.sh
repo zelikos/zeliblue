0e8c30f210b87b542974bda6a4d7d44f7f7a5eff
zelikos <pcsikos@zelikos.dev>
2025-07-05 20:08:15 -0400

    fix(image-info): Use BASE_IMAGE_NAME for PRETTY_NAME

--- build_files/shared/00-image-info.sh
+++ build_files/shared/00-image-info.sh
@@ -30,7 +30,7 @@ EOF
 
 # OS Release File
 sed -i "s/^VARIANT_ID=.*/VARIANT_ID=$IMAGE_NAME/" /usr/lib/os-release
-sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME=\"$ZELIBLUE_PRETTY_NAME (FROM Fedora ${IMAGE_FLAVOR^})\"/" /usr/lib/os-release
+sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME=\"$ZELIBLUE_PRETTY_NAME (FROM Fedora ${BASE_IMAGE_NAME^})\"/" /usr/lib/os-release
 sed -i "s/^NAME=.*/NAME=\"$ZELIBLUE_PRETTY_NAME\"/" /usr/lib/os-release
 sed -i "s|^HOME_URL=.*|HOME_URL=\"$HOME_URL\"|" /usr/lib/os-release
 sed -i "s|^DOCUMENTATION_URL=.*|DOCUMENTATION_URL=\"$DOCUMENTATION_URL\"|" /usr/lib/os-release
