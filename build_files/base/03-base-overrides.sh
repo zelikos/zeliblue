c796ab4a2d61eb9d18f11686c8382f7f9d93bf85
zelikos <pcsikos@zelikos.dev>
2025-08-03 16:15:38 -0400

    fix(base): Add workaround for just's CN README filename

--- build_files/base/03-base-overrides.sh
+++ build_files/base/03-base-overrides.sh
@@ -25,3 +25,6 @@ dnf5 distro-sync -y --repo='fedora-multimedia' "${OVERRIDES[@]}"
 
 # Replace podman provided policy.json with ublue-os one.
 mv /usr/etc/containers/policy.json /etc/containers/policy.json
+
+# Workaround: Rename just's CN readme to README.zh-cn.md
+mv '/usr/share/doc/just/README.中文.md' '/usr/share/doc/just/README.zh-cn.md'
